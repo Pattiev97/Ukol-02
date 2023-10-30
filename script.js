61103927c029578b3a6922095a7708707705f4b4
Patricie Vyhlídalová <patina.vy@gmail.com>
2023-10-30 19:53:43 +0100

    oprava zápisu podmínek

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ const pocetKusu = prompt("Zadej počet kusů, které si chceš objednat:")
 
 if(pocetKusu < 50) {
     document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 300} Kč.`
-    if(pocetKusu >= 50 && pocetKusu < 200) {
-        document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 250} Kč.`
-        if(pocetKusu >= 200 && pocetKusu < 500) {
-            document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 200} Kč.`
-            if(pocetKusu >= 500 && pocetKusu < 1000) {
-                document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 150} Kč.`
-                if(pocetKusu >= 1000) {
-                    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 120} Kč.`
-                }
-            }
-        }
-    }
+} else if(pocetKusu >= 50 && pocetKusu < 200) {
+    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 250} Kč.`
+} else if(pocetKusu >= 200 && pocetKusu < 500) {
+    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 200} Kč.`
+} else if(pocetKusu >= 500 && pocetKusu < 1000) {
+    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 150} Kč.`
+} else if(pocetKusu >= 1000) {
+    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 120} Kč.`
 }
