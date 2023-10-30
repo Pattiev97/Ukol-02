01ff0d7ce454002e2cef4a8a2175a65822616461
Patricie Vyhlídalová <patina.vy@gmail.com>
2023-10-30 19:57:34 +0100

    doplnění počtu triček a meta title

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const pocetKusu = prompt("Zadej počet kusů, které si chceš objednat:")
 
 if(pocetKusu < 50) {
-    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 300} Kč.`
+    document.body.innerHTML = `Celková cena objednávky za ${pocetKusu} triček je ${pocetKusu * 300} Kč.`
 } else if(pocetKusu >= 50 && pocetKusu < 200) {
-    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 250} Kč.`
+    document.body.innerHTML = `Celková cena objednávky za ${pocetKusu} triček je ${pocetKusu * 250} Kč.`
 } else if(pocetKusu >= 200 && pocetKusu < 500) {
-    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 200} Kč.`
+    document.body.innerHTML = `Celková cena objednávky za ${pocetKusu} triček je ${pocetKusu * 200} Kč.`
 } else if(pocetKusu >= 500 && pocetKusu < 1000) {
-    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 150} Kč.`
+    document.body.innerHTML = `Celková cena objednávky za ${pocetKusu} triček je ${pocetKusu * 150} Kč.`
 } else if(pocetKusu >= 1000) {
-    document.body.innerHTML = `Celková cena objednávky je ${pocetKusu * 120} Kč.`
+    document.body.innerHTML = `Celková cena objednávky za ${pocetKusu} triček je ${pocetKusu * 120} Kč.`
 }
+
+document.title = "Vyhlídalová - Úkol 2"
